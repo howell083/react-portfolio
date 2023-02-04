e38981ed4813be3cd64d8c1b41b571a547af12d2
J H Howell <howell083@gmail.com>
2023-02-04 17:37:13 -0600

    added basic styling

--- src/App.js
+++ src/App.js
@@ -10,17 +10,39 @@ function App() {
     <div className="App">
       
       <header style={styles.lightStyles} className="App-header">
-      <ColorPicker />
-        <ProfileForm />
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          J H Howell
-        </h1>
-        <h2>
-          Web Dev / Software Engineer
-        </h2>
-        
+
+        <div style={{width: '48%'}}>
+                <img style={{border: '3px solid #FFFDD0', borderRadius: '50%', maxWidth: 300}}src="profileSmall.jpg" alt="" />
+        </div>
+        <div style={{width: '48%'}}>
+          <h1>
+            J H Howell
+          </h1>
+          <h2>
+            Web Dev / Software Engineer
+          </h2>
+        </div>
       </header>
+      <main>
+        <ColorPicker />
+        <ProfileForm />
+            
+
+            <aside className="sidebar">
+                
+                    
+                <h3>Projects</h3>
+                
+                <p><a href="https://github.com/howell083">Github</a></p>
+                <p><a href="https://howell083.github.io">Github Pages</a></p>
+                            
+                <p>...</p>
+                            
+                
+            </aside>
+
+
+        </main>
     </div>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,15 @@
+import { findByLabelText } from "@testing-library/react"
+
 const darkStyles = {
     backgroundColor: '#141212',
     color: '#9EB599'
 }
 const lightStyles = {
     backgroundColor: '#9EB599',
-    color: '#352926'
+    color: '#352926',
+    display: 'flex',
+    justifyContent: 'space-between'
+    
 }
 const mediumStyles = {
     backgroundColor: '#352926',
